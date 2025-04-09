a9f90f886f0b164fda4eefa67bcd6548a3b4e4bc
edcan-dev <israel.da.queenie@gmail.com>
2025-04-09 10:44:33 -0600

    :sparkles: Removed questions.slice()

--- src/app/exam/start/page.tsx
+++ src/app/exam/start/page.tsx
@@ -6,7 +6,7 @@ export default function StartExamPage() {
 
   return (
     <main>
-      <QuestionsContainer questions={questions.slice(202)} />
+      <QuestionsContainer questions={questions} />
     </main>
   );
 }
